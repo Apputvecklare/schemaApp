6d22ac492a3418101e65a5a489c48709ffbdfaf4
Ashraf Jibrael <ashrafjanan@Ashrafs-MacBook-Pro.local>
2013-01-28 18:47:00 +0100

    Initial Commit

---
+++ ex-obj-c-2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ex-obj-c-2
+//
+//  Created by Ashraf Jibrael on 1/28/13.
+//  Copyright (c) 2013 Ashraf Jibrael. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
